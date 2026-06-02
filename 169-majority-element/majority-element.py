c4c260a2a2451ac9d85fc8b1232867003f33783f
Arya G Bhat <arya21bhat05@gmail.com>
2026-06-02 09:57:17 +0530

    Time: 4 ms (79.94%) | Memory: 13.7 MB (39.86%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -5,14 +5,25 @@ class Solution(object):
         :rtype: int
         """
         
-        d  = {}
-        n = len(nums)
+        # d  = {}
+        # n = len(nums)
 
-        for i in nums:
-            d[i] = d.get(i,0) + 1
+        # for i in nums:
+        #     d[i] = d.get(i,0) + 1
 
-        for i in d:
-            if d[i]>(n//2):
-                return i
+        # for i in d:
+        #     if d[i]>(n//2):
+        #         return i
         
-        return -1
+        # return -1
+        count = 0
+        maj = 0
+    
+        uni = list(dict.fromkeys(nums))
+        for i in uni:
+            if nums.count(i)>count:
+                count = nums.count(i)
+                maj = i
+
+        return maj
+
