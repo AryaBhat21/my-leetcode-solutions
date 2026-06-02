ef61165c1fa9b08f73893b9ca61b3a50d9e3a9f4
Arya G Bhat <arya21bhat05@gmail.com>
2026-06-02 10:00:52 +0530

    Time: 6 ms (71.99%) | Memory: 13.7 MB (39.86%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -4,18 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
-        # d  = {}
-        # n = len(nums)
-
-        # for i in nums:
-        #     d[i] = d.get(i,0) + 1
-
-        # for i in d:
-        #     if d[i]>(n//2):
-        #         return i
-        
-        # return -1
+    
         count = 0
         maj = 0
     
