b5e6a704b598bec9adad8124ecbb07d31e57cf33
Arya G Bhat <arya21bhat05@gmail.com>
2026-07-19 21:15:58 +0530

    Time: 3 ms (92.76%) | Memory: 22.6 MB (51.67%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,12 +1,31 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        freq = {}
+        if not nums:
+            return []
+        
+        # Phase 1: Find potential candidates
+        candidate1, candidate2 = None, None
+        count1, count2 = 0, 0
+        
+        for num in nums:
+            if num == candidate1:
+                count1 += 1
+            elif num == candidate2:
+                count2 += 1
+            elif count1 == 0:
+                candidate1, count1 = num, 1
+            elif count2 == 0:
+                candidate2, count2 = num, 1
+            else:
+                count1 -= 1
+                count2 -= 1
+        
+        # Phase 2: Verify candidates
         res = []
-        for i in nums:
-            freq[i] = freq.get(i,0)+1
-
-        for i in freq:
-            if freq[i] > len(nums)//3:
-                res.append(i)
-
+        target = len(nums) // 3
+        
+        for cand in [candidate1, candidate2]:
+            if cand is not None and nums.count(cand) > target:
+                res.append(cand)
+                
         return res
